21d79d7c97a3af8744bd0215392b12456fb6547d
yousefkassab99 <105107425+yousefkassab99@users.noreply.github.com>
2022-09-23 16:56:09 +0300

    Redux-JSX-Code

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const posts = [
+    {
+        id: 1,
+        title: "DUBAI TRAVEL OPERATOR",
+        img: "https://dubaitrippackages.files.wordpress.com/2017/11/2-imgdinosaurs_base.jpg",
+        desc: " The Worlds of Journey includes the gigantic Velociraptor, which goes at a speed of regarding 60 miles per hour, along with numerous alternative mind-blowing rides, 28 dining choices and retail stores.",
+        longDesc: " The Worlds of Journey includes the gigantic Velociraptor, which goes at a speed of regarding 60 miles per hour, along with numerous alternative mind-blowing rides, 28 dining choices and retail stores. This theme park options cartoon characters from the Marvel Comics and Cartoon Network like Spider Man, the Avengers, Power puff girls, Hulk, etc. The park is divided into 5 zones, namely- Marvel, Lost Valley, Cartoon Network, IMG Boulevard and NOVO Cinemas, and additionally presents special attractions sort of a Bollywood-theme zone, the Legoland along with a refreshing water park.."
+
+    },
+    {
+        id: 2,
+        title: "SITE IS TEMPORARILY UNAVAILABLE",
+        img: "https://media-cdn.tripadvisor.com/media/photo-s/0c/bb/a3/97/predator-ride-in-the.jpg",
+        desc: "  Effective from 30 August 2021, tourist visa applications are open to travellers from all countries. Depending on a passenger's nationality, they can get a visa on arrival, or apply for a pre-arranged visit visa from Dubai Immigration before travel. ",
+        longDesc: " Effective from 30 August 2021, tourist visa applications are open to travellers from all countries. Depending on a passenger's nationality, they can get a visa on arrival, or apply for a pre-arranged visit visa from Dubai Immigration before travel. Please reach out to The General Directorate of Residency and Foreigners Affairs and check your country’s latest travel advisory on outbound trips.  "
+
+    },
+    {
+        id: 3,
+        title: "ACCOMMODATIONS & TRAVEL",
+        img: "https://www.imgacademy.com/sites/default/files/legacyhotel.jpg",
+        desc: " Located steps away from the world-renowned IMG Academy, Legacy Hotel is a lifestyle boutique hotel available to families, parents, guests, teams, and local visitors  Located in Manatee County, Legacy Hotel is just minutes. ",
+        longDesc: " Located steps away from the world-renowned IMG Academy, Legacy Hotel is a lifestyle boutique hotel available to families, parents, guests, teams, and local visitors. Located in Manatee County, Legacy Hotel is just minutes from Bradenton, Anna Maria Island, and Sarasota and allows guests to enjoy both the on-campus programming as well as local area attractions. Rooms fill quickly during peak seasons. Book your stay to experience the best of IMG Academy!"
+
+    }
+]
